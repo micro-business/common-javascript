47ae48256ea2d04c75e744891253d9ca89839511
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2017-09-07 09:45:30 +1200

    Removed exception class

--- dist/index.js
+++ dist/index.js
@@ -1,16 +1,7 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-var _exceptions = require('./exceptions');
-
-Object.defineProperty(exports, 'Exception', {
-  enumerable: true,
-  get: function get() {
-    return _exceptions.Exception;
-  }
+Object.defineProperty(exports, '__esModule', {
+  value: true,
 });
 
 var _extensions = require('./extensions');
@@ -19,11 +10,11 @@ Object.defineProperty(exports, 'ImmutableEx', {
   enumerable: true,
   get: function get() {
     return _extensions.ImmutableEx;
-  }
+  },
 });
 Object.defineProperty(exports, 'NewSearchResultReceivedEvent', {
   enumerable: true,
   get: function get() {
     return _extensions.NewSearchResultReceivedEvent;
-  }
+  },
 });
