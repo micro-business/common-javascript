4dc2d429065aa873b4a3918dd1bf4faa04a1d69f
Morteza Alizadeh <morteza.alizadeh@gmail.com>
2018-02-02 00:26:44 +1300

    Added isNull to Common class

--- dist/Common.js
+++ dist/Common.js
@@ -14,8 +14,16 @@ Common.isUndefined = function (value) {
   return typeof value === 'undefined';
 };
 
+Common.isNotUndefined = function (value) {
+  return !Common.isUndefined(value);
+};
+
 Common.isNull = function (value) {
   return value === null;
+};
+
+Common.isNotNull = function (value) {
+  return !Common.isNull(value);
 };
 
 Common.isNullOrUndefined = function (value) {
--- src/Common.js
+++ src/Common.js
@@ -3,7 +3,11 @@
 export default class Common {
   static isUndefined = value => typeof value === 'undefined';
 
+  static isNotUndefined = value => !Common.isUndefined(value);
+
   static isNull = value => value === null;
+
+  static isNotNull = value => !Common.isNull(value);
 
   static isNullOrUndefined = value => Common.isUndefined(value) || Common.isNull(value);
 }
